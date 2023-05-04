87f84cee4bf0376a34d23da4b48fc86555ebe4c3
nama.gupta <nama.gupta@globallogic.com>
2023-05-04 10:03:05 +0530

    Created rating feign client.

--- user-service/src/main/java/com/rating/user/client/RatingService.java
+++ user-service/src/main/java/com/rating/user/client/RatingService.java
@@ -3,22 +3,19 @@ package com.rating.user.client;
 import com.rating.user.entity.Rating;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "RATING-SERVICE")
+@Service
 public interface RatingService {
 
     @PostMapping("/api/v1/ratings")
-    public ResponseEntity<Rating> createRating(Rating values);
-
-
-    //PUT
+    ResponseEntity<Rating> createRating(Rating values);
     @PutMapping("/api/v1/ratings/{ratingId}")
-    public ResponseEntity<Rating> updateRating(@PathVariable("ratingId") String ratingId, Rating rating);
-
-
+    ResponseEntity<Rating> updateRating(@PathVariable("ratingId") String ratingId, Rating rating);
     @DeleteMapping("/api/v1/ratings/{ratingId}")
-    public void deleteRating(@PathVariable String ratingId);
+    void deleteRating(@PathVariable String ratingId);
 }
--- user-service/src/main/java/com/rating/user/entity/Rating.java
+++ user-service/src/main/java/com/rating/user/entity/Rating.java
@@ -1,12 +1,27 @@
 package com.rating.user.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Rating {
     private String ratingId;
     private String userId;
     private String hotelId;
     private int rating;
+    private String feedback;
     private Hotel hotel;
+
+    public Rating(String userId, String hotelId, int rating, String feedback, Hotel hotel) {
+        this.userId = userId;
+        this.hotelId = hotelId;
+        this.rating = rating;
+        this.feedback = feedback;
+        this.hotel = hotel;
+    }
 }
